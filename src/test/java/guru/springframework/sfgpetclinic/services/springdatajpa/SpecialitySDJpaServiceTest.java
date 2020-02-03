42b2991de9ac47a8c9141e8be794f7fc55012568
Nick Gorajski <nick_gorajski@bcbsil.com>
2020-02-03 15:19:52 -0600

    Add examples of mock verify - SpecialitySDJpaServiceTest

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -20,8 +22,27 @@ class SpecialitySDJpaServiceTest {
     SpecialitySDJpaService service;
 
     @Test
-    void testDeleteById() {
-        service.deleteById(1l);
+    void testDeleteById_SingleInvocationSuccess() {
+        service.deleteById(1L);
+
+        verify(specialtyRepository).deleteById(1L);
+    }
+
+    @Test
+    void testDeleteById_DoubleInvocationFailure() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository).deleteById(1L);     // Mockito defaults to one call only.
+                                                        // If you want more, you need to be explicit as shown below.
+    }
+
+    @Test
+    void testDeleteById_DoubleInvocationSuccess() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, times(2)).deleteById(1L);
     }
 
     @Test
